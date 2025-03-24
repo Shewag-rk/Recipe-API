75f5bdc8e15c32dd47b676b768d9eed9258b3b07
shewag-rk <shewagsurya02@gmail.com>
2025-03-24 17:13:36 +0530

    Half-Completed

---
+++ script.js
@@ -0,0 +1,72 @@
+const carousel = document.getElementById('carousel');
+const moreRecipebtn = document.querySelector('.more-recipes-btn');
+const recipeList = document.querySelector('.recipe-list');
+const apiKey = "4d4601e174c54ed597965c200dba685d";
+let itemArray = [];
+
+
+document.addEventListener("DOMContentLoaded", (event)=>{
+    event.preventDefault();
+
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`)
+        .then(response =>{
+            if(response.status.ok){
+                throw new Error(`Network response was not ok: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data);
+
+            const recipeItem = data.results
+
+            console.log(recipeItem);
+
+            carousel.innerHTML = `<i class="fa-solid fa-arrow-left icon-btn left-icon"></i>`;
+
+            recipeItem.forEach(item => {
+                carousel.innerHTML +=`
+                <img src=${item.image}></img>
+                `
+            });
+        
+            carousel.innerHTML += `<i class="fa-solid fa-arrow-right icon-btn right-icon"></i>`;
+
+            recipeItem.forEach(item=>{
+                recipeList.innerHTML +=`
+                <div class="recipe-item">
+                    <img src=${item.image}></img>
+                    <h5>${item.title}</h5>
+                </div>
+                `
+            })
+
+            moreRecipebtn.addEventListener('click', function(){
+                fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=10`)
+                .then(response =>{
+                    if(response.status.ok){
+                        throw new Error(`Network response was not ok: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(moreRecipe =>{
+                    console.log(moreRecipe);
+                    const recipeDetails = moreRecipe.recipes
+                    console.log(recipeDetails);
+
+                    recipeDetails.forEach(item=>{
+                        recipeList.innerHTML +=`
+                        <div class="recipe-item">
+                            <img src=${item.image}></img>
+                            <h5>${item.title}</h5>
+                        </div>
+                        `
+                })
+            })
+
+        })
+    })
+    .catch(error =>{
+        console.log('Fetch error', error);
+    })
+})
